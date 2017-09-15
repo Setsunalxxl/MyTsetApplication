90cf417104430e500f869b91de441cd4386b4a00
Setsunal <Setsunal@outlook.com>
2017-09-15 09:08:42 +0800

    test branch

--- app/src/main/java/setsunal/com/mytsetapplication/FullscreenActivity.java
+++ app/src/main/java/setsunal/com/mytsetapplication/FullscreenActivity.java
@@ -47,6 +47,10 @@ public class FullscreenActivity extends AppCompatActivity {
                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+
+            
+
         }
     };
     private View mControlsView;
