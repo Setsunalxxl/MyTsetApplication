c07ce9de50759d869b8156226ae97f9992499663
Setsunal <Setsunal@outlook.com>
2017-11-01 16:49:08 +0800

    1111111111

--- app/src/main/java/setsunal/com/mytsetapplication/SecondEmptyActivity.kt
+++ app/src/main/java/setsunal/com/mytsetapplication/SecondEmptyActivity.kt
@@ -8,6 +8,10 @@ class SecondEmptyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second_empty)
+
+
+
+
         val a = 10 / 0
     }
 }
